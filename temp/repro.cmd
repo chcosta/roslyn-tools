f65faf6a5dc43f7b56ebe42249dc58cb1d103a91
Christopher Costa <chcosta@microsoft.com>
2018-01-19 11:49:34 -0800

    Move bootstrap into src folder

--- temp/repro.cmd
+++ temp/repro.cmd
@@ -1,1 +1,1 @@
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "&([scriptblock]::Create((Invoke-WebRequest -useb 'https://raw.githubusercontent.com/chcosta/roslyn-tools/bootstrap/build/bootstrap/obtain/toolset-install.ps1')))
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "&([scriptblock]::Create((Invoke-WebRequest -useb 'https://raw.githubusercontent.com/chcosta/roslyn-tools/bootstrap/src/bootstrap/obtain/toolset-install.ps1')))
